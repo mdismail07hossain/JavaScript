f1d877be7edb03487bdebcda7e824982c6991212
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-10-17 23:49:58 +0600

    java

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 var num = 32;
 var num2 = 45;
 console.log(num);
+
+document.write("hello Ismail");
+
+function myfuntion() {
+  document.getElementById("BgColor").style.background = "black";
+  document.getElementById("BgColor").style.color = "white";
+}
+function whitefuntion() {
+  document.getElementById("BgColor").style.background = "red";
+  document.getElementById("BgColor").style.color = "white";
+}
+function redfuntion() {
+  document.getElementById("BgColor").style.background = "white";
+  document.getElementById("BgColor").style.color = "black";
+}
