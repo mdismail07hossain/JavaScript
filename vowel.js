8d571c744982a4cc99da170c7aa6c1d6a4cb7797
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-10-24 22:54:12 +0600

    java

---
+++ vowel.js
@@ -0,0 +1,13 @@
+// find in vowle letter
+let letter = prompt("letter:");
+switch (true) {
+  case letter === "a" ||
+    letter === "e" ||
+    letter === "i" ||
+    letter === "o" ||
+    letter === "u":
+    window.alert("this is vowle");
+    break;
+  default:
+    window.alert("NOt vowle");
+}
