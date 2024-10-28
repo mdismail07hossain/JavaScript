a0005b4c4ed7183a7fb1b1ce06e2c53f9adfa0ba
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-10-28 23:27:50 +0600

    java

---
+++ jsprectices.js
@@ -0,0 +1,29 @@
+/*const out = function () {
+  window.alert("hello wolrd");
+};
+
+console.log("hello");
+
+var apples = 2.0;
+var oranges = 2.34;
+var pears = 3.3;
+var tax = 0.04;
+var shipping = 3.43;
+var subtotal = apples + oranges + pears;
+var total = subtotal + subtotal * tax + shipping;
+var message = "Your total is $";
+var deliver = message + total + ".";
+document.write(deliver);
+var alpha = 0xdead;
+var beta = 0xbeef;
+var gamma = (alpha + beta).toString(10);
+document.write(gamma);
+var str = 45 % 2 == 0 ? "hello" : "bye";*/
+// document.write(str);
+var a = 7;
+a += 12;
+a -= 4;
+a *= 5;
+a /= 3;
+a %= 2;
+console.log(a);
