55c2c9aac29563a0bf99a526b89ac45c11e1556b
mdismail07hossain <mdismail07.hossain@gmail.com>
2024-11-08 21:25:26 +0600

    java

---
+++ Java_Variable/variable.js
@@ -0,0 +1,13 @@
+// Variable declare by var method
+var a = 12;
+var b = 13;
+var a = 20;
+var total = a + b;
+console.log(total);
+console.log(total + 12);
+// Variable declare by let
+
+let c = 13;
+c = 30;
+c = 45;
+console.log(c);
